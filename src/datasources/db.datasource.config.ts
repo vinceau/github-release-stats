2a13b293e2e054f7bbd2c3a86fcf982fa7722ab4
Vince Au <vinceau09@gmail.com>
2020-06-03 23:40:01 +1000

    Prefer DATABASE_URL if defined

--- src/datasources/db.datasource.config.ts
+++ src/datasources/db.datasource.config.ts
@@ -1,10 +1,21 @@
-export const config = {
-  name: 'db',
-  connector: 'postgresql',
+const baseConfig = {
+  name: "db",
+  connector: "postgresql",
+  ssl: { rejectUnauthorized: false },
+};
+
+const urlConfig = {
+  ...baseConfig,
+  url: process.env.DATABASE_URL,
+};
+
+const standardConfig = {
+  ...baseConfig,
   host: process.env.PSQL_HOST,
   port: process.env.PSQL_PORT,
   user: process.env.PSQL_USER,
   password: process.env.PSQL_PASSWORD,
   database: process.env.PSQL_DATABASE,
-  ssl: false,
 };
+
+export const config = process.env.DATABASE_URL ? urlConfig : standardConfig;
