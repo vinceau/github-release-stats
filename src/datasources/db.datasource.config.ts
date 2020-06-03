dcaf5f6a3aca0c3998d97338a07fe0d901e27495
Vince Au <vinceau09@gmail.com>
2020-06-03 21:51:51 +1000

    Separate dev and prod configurations

--- src/datasources/db.datasource.config.ts
+++ src/datasources/db.datasource.config.ts
@@ -1,6 +1,8 @@
-export const config = {
-  name: 'db',
-  connector: 'postgresql',
+console.log("node env: " + process.env.NODE_ENV);
+
+const prodConfig = {
+  name: "db",
+  connector: "postgresql",
   host: process.env.PSQL_HOST,
   port: process.env.PSQL_PORT,
   user: process.env.PSQL_USER,
@@ -8,3 +10,16 @@ export const config = {
   database: process.env.PSQL_DATABASE,
   ssl: false,
 };
+
+const devConfig = {
+  name: "db",
+  connector: "postgresql",
+  host: "localhost",
+  port: "5432",
+  user: "user",
+  password: "password",
+  database: "postgres",
+  ssl: false,
+};
+
+export const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
