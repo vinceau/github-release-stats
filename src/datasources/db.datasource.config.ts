125a0ed23db0cbbb8a2282c7da20078aa3bd2c8f
Vince Au <vinceau09@gmail.com>
2020-06-03 22:36:03 +1000

    Revert "Separate dev and prod configurations"
    This reverts commit dcaf5f6a3aca0c3998d97338a07fe0d901e27495.

--- src/datasources/db.datasource.config.ts
+++ src/datasources/db.datasource.config.ts
@@ -1,8 +1,6 @@
-console.log("node env: " + process.env.NODE_ENV);
-
-const prodConfig = {
-  name: "db",
-  connector: "postgresql",
+export const config = {
+  name: 'db',
+  connector: 'postgresql',
   host: process.env.PSQL_HOST,
   port: process.env.PSQL_PORT,
   user: process.env.PSQL_USER,
@@ -10,16 +8,3 @@ const prodConfig = {
   database: process.env.PSQL_DATABASE,
   ssl: false,
 };
-
-const devConfig = {
-  name: "db",
-  connector: "postgresql",
-  host: "localhost",
-  port: "5432",
-  user: "user",
-  password: "password",
-  database: "postgres",
-  ssl: false,
-};
-
-export const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
